52c3c7098abb386f7f7b9010b98e40d2bf041b57
Radosław Wierzgała <rad.wierzgala@gmail.com>
2023-08-20 19:21:02 +0200

    SQL queryu updated, added date_to, date_from filtering

--- wizards/all_products_wizard.py
+++ wizards/all_products_wizard.py
@@ -13,11 +13,53 @@ class AllProductsReportWizard(models.TransientModel):
     date_to = fields.Date(string="Date To")
 
     def get_all_products(self, date_from, date_to):
-        query = """
-            SELECT name, list_price
-            FROM product_template
-            ORDER BY name
+        #napisac mehcanizm zamiany date_to z date_from jeżeli są date_to < date_from
+        date_from = "'" + str(date_from) + "'"
+        date_to = "'" + str(date_to) + "'"
+
+        # DZIAŁA XD
+        # query = """
+        #     SELECT 
+        #         pt.name, 
+        #         SUM(aml.price_subtotal) as total_price_subtotal,
+        #         SUM(aml.quantity) as qty 
+        #     FROM account_move am
+        #     JOIN account_move_line aml ON am.id = aml.move_id
+        #     JOIN product_product pp ON aml.product_id = pp.id
+        #     JOIN product_template pt ON pp.product_tmpl_id = pt.id
+        #     WHERE move_type = 'out_invoice' AND state='posted'
+        #     GROUP BY pt.name
+        #     ORDER BY total_price_subtotal DESC;
+        # """
+
+        query = f"""
+            SELECT 
+                pt.name, 
+                SUM(aml.price_subtotal) as total_price_subtotal,
+                SUM(aml.quantity) as qty 
+            FROM account_move am
+            JOIN account_move_line aml ON am.id = aml.move_id
+            JOIN product_product pp ON aml.product_id = pp.id
+            JOIN product_template pt ON pp.product_tmpl_id = pt.id
+            WHERE am.move_type = 'out_invoice' AND am.state='posted' AND am.invoice_date >= {date_from} AND am.invoice_date <= {date_to}
+            GROUP BY pt.name
+            ORDER BY total_price_subtotal DESC;
         """
+
+ 
+        # query = f"""
+        #     SELECT 
+        #         pt.name, 
+        #         SUM(aml.price_subtotal) as total_price_subtotal,
+        #         SUM(aml.quantity) as qty 
+        #     FROM account_move am
+        #     JOIN account_move_line aml ON am.id = aml.move_id
+        #     JOIN product_product pp ON aml.product_id = pp.id
+        #     JOIN product_template pt ON pp.product_tmpl_id = pt.id
+        #     WHERE am.move_type = 'out_invoice' AND am.state='posted' AND am.invoice_date >= '2023-08-01' AND am.invoice_date <= '2023-08-30'
+        #     GROUP BY pt.name
+        #     ORDER BY total_price_subtotal DESC;
+        # """
         
         cr = self.env.cr
         cr.execute(query)
@@ -32,11 +74,10 @@ class AllProductsReportWizard(models.TransientModel):
         date_from = self.read()[0]['date_from']
         date_to = self.read()[0]['date_to']
         products = self.get_all_products(date_from, date_to)
-        _logger.warning(f"@@@@@@@@@@@@@@@@@@@@@@@ LOGGER RW: products - {products}")
         data = {
             'products': products,
-            # 'form_data': self.read()[0],
             'date_from': date_from,
-
+            'date_to': date_to,
         }
         return self.env.ref('sales_analysis_rw.action_all_products_report_print').report_action(self, data=data)
+        
